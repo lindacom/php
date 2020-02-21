f24239e3271473ae3f8df3044803e70df5660425
lindacom <43495865+lindacom@users.noreply.github.com>
2020-02-21 11:41:15 +0000

    Update url-db-query.php

--- url-db-query.php
+++ url-db-query.php
@@ -15,17 +15,28 @@
 
 ?>
 
-<!--QUERY DATABASE FOR BOOK LIKE THE SEARCH VALUE --> 
+<!--QUERY DATABASE FOR BOOK LIKE THE SEARCH VALUE AND DISPLAY ON PAGE --> 
 
 <?php
 
-$stmt= "SELECT title, description FROM books WHERE title LIKE '%".$page_name."%' ";
+
+//Read Query from books database
+
+$stmt= "SELECT title, description, image, price FROM books WHERE title LIKE '%".$page_name."%' ";
 $result = $connect->query($stmt);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo " - title: " . $row["title"]. " " . $row["description"]. "<br>";
+
+        echo    '<div class="box">';
+        echo    '<object data='.$row["image"].' width="50%"  class="group list-group-image">
+    <img src="http://placehold.it/400x250/000/fff" width="50%"/>
+  </object>';
+   echo    '<h2>'.$row["title"].'</h2>';
+ echo    '<h2>'.$row["description"].'</h2>';
+    echo    '<a href="http://lindacom.infinityfreeapp.com/books/store.php?action=get&title='.$row["title"].'&price='.$row["price"].'&quantity=1 "target="_blank" class="button"><i class="fa fa-shopping-cart"></i>Buy</a>';
+   echo    '</div>';
     }
 } else {
     echo "0 results";
